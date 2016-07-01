a71b5cf788f4ea44b4806101c0060ef43f84bb29
Mikel Vuka <mikelv92@gmail.com>
2016-07-01 16:37:35 +0200

    forced orientation

--- regression/src/main.cpp
+++ regression/src/main.cpp
@@ -27,6 +27,8 @@ void moveBase(Position position)
 
 	goal.target_pose.pose.position.x = position.getX();
 	goal.target_pose.pose.position.y = position.getY();
+	goal.target_pose.pose.orientation.z = 1;
+	goal.target_pose.pose.orientation.w = 0;
 
 	ROS_INFO("Sending goal");
 	moveBaseClient.sendGoal(goal);
